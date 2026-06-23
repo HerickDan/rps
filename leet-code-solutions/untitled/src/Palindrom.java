d33b4dba1c56f6610c87648abcc2e92b8818c22b
HerickDan <herickdan@hotmail.com>
2026-06-23 18:41:40 -0300

    solutions

---
+++ leet-code-solutions/untitled/src/Palindrom.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Palindrom {
+       public static void (){
+           double dbNumber = 25.5;
+           int number = 21;
+           Inte
+       }
+}

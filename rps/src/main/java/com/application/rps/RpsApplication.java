638e7ddfa69aeaab414d6aa92d6f30b5d3d59178
HerickDan <herickdan@hotmail.com>
2026-07-06 15:59:02 -0300

    Another configs

--- rps/src/main/java/com/application/rps/RpsApplication.java
+++ rps/src/main/java/com/application/rps/RpsApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RpsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RpsApplication.class, args);
-        System.out.println("RUNING");
+        System.out.println("RUNNING");
 	}
-
 }

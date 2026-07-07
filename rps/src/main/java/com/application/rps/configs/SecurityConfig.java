bfb3797c8c325ba7bef341260f9c56f87b0d5b89
HerickDan <herickdan@hotmail.com>
2026-07-06 21:38:04 -0300

    feat: security config

---
+++ rps/src/main/java/com/application/rps/configs/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.application.rps.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) {
+        return http.csrf(it -> it.disable())
+                .cors(it -> it.disable())
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(HttpMethod.GET, "/player/**").permitAll().
+                                requestMatchers(HttpMethod.POST, "/player/**").permitAll().
+                                requestMatchers(HttpMethod.PATCH, "/player/**").permitAll().
+                                requestMatchers(HttpMethod.DELETE, "/player/**").permitAll()
+                ).build();
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}

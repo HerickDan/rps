c6214cf70412382d78124450b8ee224595b396e3
HerickDan <herickdan@hotmail.com>
2026-07-08 17:33:19 -0300

    feat: adjusting the interfaces

--- src/main/java/com/application/rps/services/interfaces/IPlayerService.java
+++ src/main/java/com/application/rps/services/interfaces/IPlayerService.java
@@ -2,7 +2,9 @@ package com.application.rps.services.interfaces;
 
 
 import com.application.rps.commons.dto.CreatePlayerDto;
+import com.application.rps.repository.entity.PlayerEntity;
 
 public interface IPlayerService {
-    public default void createPlayer(CreatePlayerDto dto){}
+    void createPlayer(CreatePlayerDto dto);
+    PlayerEntity findByPlayerByName(String name);
 }
